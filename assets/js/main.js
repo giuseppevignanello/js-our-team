e2c40890321a36c39194dbc73ec945a09f4562e2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-22 15:28:36 +0100

    add array

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,17 +16,49 @@
 
 // MILESTONE 0
 // Create an array with six objects insiede
+const team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO", 
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor", 
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager", 
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager", 
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer", 
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer", 
+        img: "barbara-ramos-graphic-designer.jpg"
+    },
 
+]
 
 // MILESTONE 1
-//console.log the keys and values of each object with a for loop 
+//console.log the keys and values of each object with a for loop
 
-//MILESTONE 2 
-// Take an element from the DOM 
-//inner the keys and values of each object with for loop and templete literal 
+//MILESTONE 2
+// Take an element from the DOM
+//inner the keys and values of each object with for loop and templete literal
 
-// BONUS 1 
-// add img element with for loop inner html and template literal 
+// BONUS 1
+// add img element with for loop inner html and template literal
 
-//BONUS 2 
+//BONUS 2
 // add complete HTML with for loop, inner HTML and template literl 
